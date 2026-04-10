447e189d616d63c762c28ca1c914a2061b66c01b
2Grifon <evgeny.suraykin@gmail.com>
2026-04-10 18:47:39 +0300

    upd - Added test user credentials to README and default.env, and created seed script for user, admin and user account

---
+++ backend/alembic/versions/27093299c4d3_seed_user_admin_and_account.py
@@ -0,0 +1,100 @@
+"""Seed user, admin and account
+
+Revision ID: 27093299c4d3
+Revises: e2f418f376f4
+Create Date: 2026-04-10 15:26:40.306781
+
+Default credentials
+
+User:  user@example.com  / check .env for password
+Admin: admin@example.com / check .env for password
+
+"""
+
+from datetime import datetime, timezone
+import os
+from typing import Sequence, Union
+import uuid
+
+from alembic import op
+import sqlalchemy as sa
+
+from pwdlib import PasswordHash
+from pwdlib.hashers.argon2 import Argon2Hasher
+from pwdlib.hashers.bcrypt import BcryptHasher
+
+
+# revision identifiers, used by Alembic.
+revision: str = "27093299c4d3"
+down_revision: Union[str, Sequence[str], None] = "e2f418f376f4"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+password_hash = PasswordHash((Argon2Hasher(), BcryptHasher()))
+
+
+def now() -> datetime:
+    return datetime.now(timezone.utc)
+
+
+# Фиксированные UUID — чтобы account мог ссылаться на user
+USER_ID = uuid.uuid4()
+ADMIN_ID = uuid.uuid4()
+ACCOUNT_ID = uuid.uuid4()
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    ts = now()
+
+    bind = op.get_bind()
+    meta = sa.MetaData()
+    meta.reflect(bind=bind)
+
+    user_table = meta.tables["user"]
+    account_table = meta.tables["account"]
+
+    op.bulk_insert(
+        user_table,
+        [
+            {
+                "id": USER_ID,
+                "email": "user@example.com",
+                "hashed_password": password_hash.hash(os.getenv("MOCK_USER_PASSWORD")),
+                "full_name": "Test User",
+                "is_active": True,
+                "role": "user",
+                "created_at": ts,
+                "updated_at": ts,
+            },
+            {
+                "id": ADMIN_ID,
+                "email": "admin@example.com",
+                "hashed_password": password_hash.hash(os.getenv("MOCK_ADMIN_PASSWORD")),
+                "full_name": "Test Admin",
+                "is_active": True,
+                "role": "admin",
+                "created_at": ts,
+                "updated_at": ts,
+            },
+        ],
+    )
+
+    op.bulk_insert(
+        account_table,
+        [
+            {
+                "id": ACCOUNT_ID,
+                "user_id": USER_ID,
+                "balance": "0.00",
+                "created_at": ts,
+                "updated_at": ts,
+            },
+        ],
+    )
+    pass
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    pass
